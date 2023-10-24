47712f81f37edc8eec2c35031f2387e85d88ce5c
Muthu Annamalai <photonique@hey.com>
2023-10-24 00:24:17 -0700

    - add generalize Taguchi method for more factors when the number of factors doesn`t match the table exactly   -- this may result in runs which are duplicated but still within Orthogonal Array property - add unittests in pytest framework - indent with yapf -i - setup pipelines - fix fmt of YAML actions - fix regr - drop cols at once - add requirements files - update reqs

--- example.py
+++ example.py
@@ -1,28 +1,16 @@
 from pyTaguchi.taguchi import Taguchi
 
-var1 = {
-  "name": "Variable n.1",
-  "values": [30, 35, 40]
-}   
+var1 = {"name": "Variable n.1", "values": [30, 35, 40]}
 
-var2 = {
-  "name": "Variable n.2",
-  "values": [20, 30, 40]
-}  
+var2 = {"name": "Variable n.2", "values": [20, 30, 40]}
 
-var3 = {
-  "name": "Variable n.3",
-  "values": [10, 27, 50]
-}  
+var3 = {"name": "Variable n.3", "values": [10, 27, 50]}
 
-var4 = {
-  "name": "Variable n.4",
-  "values": [0.2, 0.5, 0.7]
-}      
+var4 = {"name": "Variable n.4", "values": [0.2, 0.5, 0.7]}
 
 tg = Taguchi()
 tg.add(var1)
 tg.add(var2)
 tg.add(var3)
 tg.add(var4)
-tg.run(randomize=True)
+tg.run(randomize=True,plot=True)
